62dbdf65abe1384d98355695a180121d4daeba75
Saiful Islam <saiful islam>
2025-06-21 15:36:02 +0600

    remove dist and env files from git tracking

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,6 @@
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:8080",
-    },
-  },
-});
+  plugins: [tailwindcss()],
+})
